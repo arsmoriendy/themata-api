c96427fe4f0b7fbdd80d19641442e1803da12026
arsmoriendy <nugrahasteven@gmail.com>
2026-02-01 07:29:06 +0700

    feat: handle theme count overflow

--- src/handlers/create.rs
+++ src/handlers/create.rs
@@ -10,9 +10,19 @@ pub async fn create(
     ValidSession(user_ulid): ValidSession,
     State(AppState { db }): State<AppState>,
     Valid(AxumJson(create_data)): Valid<AxumJson<CreateData>>,
-) -> Result<String, StatusCode> {
+) -> Result<String, (StatusCode, &'static str)> {
     db.create_theme(&create_data, &user_ulid)
         .await
         .map(|u| u.0.to_string())
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+        .map_err(|e| {
+            if let Some(e) = e.into_database_error() {
+                let Some(e) = e.constraint() else {
+                    return (StatusCode::INTERNAL_SERVER_ERROR, "database error");
+                };
+                if e == "max_themes" {
+                    return (StatusCode::CONFLICT, "user exceeded maximum theme quota");
+                }
+            }
+            (StatusCode::INTERNAL_SERVER_ERROR, "database protocol error")
+        })
 }
